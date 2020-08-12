b39bfe5d81cfe19a24a26d94ee9f0fbfe7adf5fc
SeoHoJun <ghwns6659@naver.com>
2020-08-12 16:52:37 +0900

    Swagger Customizing

--- src/main/java/com/practice/restfulwebservice/config/SwaggerConfig.java
+++ src/main/java/com/practice/restfulwebservice/config/SwaggerConfig.java
@@ -2,16 +2,35 @@ package com.practice.restfulwebservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    // 연락처 정보를 위한 Contact 객체 생성(이름, url, 이메일)
+    private static final Contact DEFAULT_CONTACT = new Contact("Prid", "http://www.joneconsulting.co.kr", "edowon@joneconsulting.co.kr");
 
+    // 상수로 만들고 있는 목적은, 항목 고정이 되면 변경할 필요가 없는 정보들이기 때문에, Contact 나 ApiInfo 같은 정보를 상수로 만들어 두는 것이 편하다.
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome API Title", "My User management REST API service", "1.0", "urn : tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/license/LICENSE-2.0", new ArrayList<>());
+
+    // 프로듀스와 컨슈머가 어떤 형태로 데이터를 사용할 수 있는지 문서 타입을 지정한다.
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(Arrays.asList("application/json", "application/xml"));
+    // Arrays.asList() 메소드는 배열 형태의 데이터를 리스트 형태로 변환시켜 준다.
     @Bean
     public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2);
+        // 아래의 Docket 객체에서 api 정보, 프로듀스 정보, 컨슈머 정보를 변경해보자.
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
     }
 }
--- src/main/java/com/practice/restfulwebservice/user/User.java
+++ src/main/java/com/practice/restfulwebservice/user/User.java
@@ -1,6 +1,8 @@
 package com.practice.restfulwebservice.user;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,14 +16,19 @@ import java.util.Date;
 @NoArgsConstructor
 //@JsonFilter("UserInfo")
 //@JsonIgnoreProperties(value = {"password", "ssn"})
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체") // Swagger 커스터마이징
 public class User {
     private Integer id;
 
     @Size(min = 2, message = "Name 은 2글자 이상 입력해주세요") // 속성 값의 길이가 최소 2를 넘어야 한다는 제약 부여
+    @ApiModelProperty(notes = "사용자 이름을 입력해주세요.") // Swagger 커스터마이징
     private String name;
     @Past // 과제 데이터만 사용할 수 있다는 제약 부여
+    @ApiModelProperty(notes = "등록일 을 입력해주세요.")
     private Date joinDate;
 
+    @ApiModelProperty(notes = "사용자의 패스워드를 입력해주세요.")
     private String password;
+    @ApiModelProperty(notes = "사용자의 주민번호를 입력해주세요.")
     private String ssn;
 }
