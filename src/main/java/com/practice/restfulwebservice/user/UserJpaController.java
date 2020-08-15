4e311c66fd76dc49f16df29cdfed4478ec617be2
SeoHoJun <ghwns6659@naver.com>
2020-08-16 00:29:00 +0900

    JPA, individual search

--- src/main/java/com/practice/restfulwebservice/user/UserJpaController.java
+++ src/main/java/com/practice/restfulwebservice/user/UserJpaController.java
@@ -1,11 +1,18 @@
 package com.practice.restfulwebservice.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/jpa") // uri prefix 지정
@@ -16,5 +23,23 @@ public class UserJpaController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers(){
        return userRepository.findAll(); // 특별한 조건없이 모든 데이터를 반환한다.
+    }
+
+    @GetMapping("/users/{id}")
+    public Resource<User> retrieveUser(@PathVariable int id){
+        Optional<User> user = userRepository.findById(id);
+
+        if(!user.isPresent()){ // 해당하는 id 값의 학생정보가 존재하지 않을 경우 예외 반환
+            throw new UserNOTFoundException(String.format("ID[%s] not found"));
+        }
+        // 만약 HATEOAS 기능을 사용하고 싶다면?
+        Resource<User> resource = new Resource<>(user.get());
+        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        resource.add(linkTo.withRel("all-users")); // resource 에 링크 추가
+
+        return resource;
+        //return user.get(); // 데이터 반환값이 Optional 타입이라고 메소드의 반환 타입을 똑같이 맞춰주려고 할 필요 없이
+        // 위와 같이 get() 메소드를 활용해주면 된다.
+        //get() 메소드는 T 타입의 반환값, 즉 위의 user 데이터 초기화에서 User 타입의 데이터를 가진다.
     }
 }
